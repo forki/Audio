53aa669dde4c48e306053bc3213de6a19760234c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2018-10-05 10:41:27 +0200

    Bumping version to 0.0.12

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,13 +1,13 @@
 module internal ReleaseNotes
 
-let Version = "0.0.10"
+let Version = "0.0.12"
 
 let IsPrerelease = false
 
 let Notes = """
 # Release Notes
 
-## 0.0.10 - 2018-10-05
+## 0.0.12 - 2018-10-05
 
 * Initial release
 """

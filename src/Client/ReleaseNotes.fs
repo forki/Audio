089443dcb8d11115d5d498183f26ad34c3461c97
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2018-10-17 16:18:20 +0200

    Bumping version to 0.7.10

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,13 +1,13 @@
 module internal ReleaseNotes
 
-let Version = "0.7.9"
+let Version = "0.7.10"
 
 let IsPrerelease = false
 
 let Notes = """
 # Release Notes
 
-## 0.7.9 - 2018-10-17
+## 0.7.10 - 2018-10-17
 * Youtube player
 
 ## 0.5.7 - 2018-10-17

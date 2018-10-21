0e101f561c522cdf9dcd619947c25143ad1ed3c9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2018-10-21 21:57:43 +0200

    Bumping version to 0.10.22

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,13 +1,13 @@
 module internal ReleaseNotes
 
-let Version = "0.10.21"
+let Version = "0.10.22"
 
 let IsPrerelease = false
 
 let Notes = """
 # Release Notes
 
-## 0.10.21 - 2018-10-18
+## 0.10.22 - 2018-10-18
 * Use better UI
 
 ## 0.9.7 - 2018-10-18

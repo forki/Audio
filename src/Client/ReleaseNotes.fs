ae6e3bec8f0da0f22772f4f7a066737ee0ac7230
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2018-12-15 12:13:22 +0100

    Bumping version to 0.16.10

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,13 +1,13 @@
 module internal ReleaseNotes
 
-let Version = "0.16.9"
+let Version = "0.16.10"
 
 let IsPrerelease = false
 
 let Notes = """
 # Release Notes
 
-## 0.16.9 - 2018-12-15
+## 0.16.10 - 2018-12-15
 * Update to ASP.NET 2.2
 
 ## 0.15.11 - 2018-11-05

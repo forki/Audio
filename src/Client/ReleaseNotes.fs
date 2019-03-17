03239afecc6f222b86b0055694b848f993fe5187
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2019-03-17 08:22:40 +0100

    Bumping version to 1.1.13

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,13 +1,13 @@
 module internal ReleaseNotes
 
-let Version = "1.1.12"
+let Version = "1.1.13"
 
 let IsPrerelease = false
 
 let Notes = """
 # Release Notes
 
-## 1.1.12 - 2019-03-16
+## 1.1.13 - 2019-03-17
 * Upgrade Raspbian
 
 ## 1.1.0 - 2019-01-20

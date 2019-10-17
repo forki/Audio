1646ccff9f528a1303c0ac52ecc310a18b3b1371
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2019-10-17 13:50:12 +0200

    Bumping version to 1.7.4

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,13 +1,13 @@
 module internal ReleaseNotes
 
-let Version = "1.7.3"
+let Version = "1.7.4"
 
 let IsPrerelease = false
 
 let Notes = """
 # Release Notes
 
-## 1.7.3 - 2019-10-17
+## 1.7.4 - 2019-10-17
 * .NET Core 3
 
 ## 1.6.1 - 2019-10-16
